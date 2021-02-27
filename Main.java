7b3ea013bfff052948cf3a8035cc6119649f9d80
Brendan T Pritikin <56364353+brendantpritikin@users.noreply.github.com>
2021-02-27 14:13:31 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+    /**
+     testing IDE with a simple method
+     */
+    public static void myMethod(String myString) {
+        System.out.println(myString);
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        String myString = new String();
+        myString = "\nHola compadres\n";
+        myMethod(myString);
+    }
+}
