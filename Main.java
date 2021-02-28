e409fb69ab6b930efe865ea1444ceeaa955b80d4
Brendan T Pritikin <brendan.pritikin@gmail.com>
2021-02-28 00:11:26 +0000

    Test commit

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ class Main {
 
     public static void main(String[] args) {
         String myString = new String();
-        myString = "\nHola compadres\n";
+        myString = "\nHello there\n";
         myMethod(myString);
     }
 }
